58a24edc7b20a2b9403ceae81e12e78bd1e60a73
ivissivi <ivis31121999@gmail.com>
2024-02-25 16:43:44 +0200

    Clean-up and remove redundant code, clear spaces

--- ar_cv.py
+++ ar_cv.py
@@ -3,12 +3,9 @@ import numpy as np
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
-
 phone_camera_url = 'http://192.168.101.15:8080/video'
 camera = cv2.VideoCapture(phone_camera_url)
-
 color_to_track = np.array([30, 150, 50])
-
 piano_keys = ["C", "D", "E", "F", "G", "A", "B", "C"]
 
 def generate_frames():
@@ -16,36 +13,28 @@ def generate_frames():
         success, frame = camera.read()
         if not success:
             text_frame = np.zeros((480, 640, 3), dtype=np.uint8)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(text_frame, 'No input available', (50, 240), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
+            cv2.putText(text_frame, 'No input available', (50, 240), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
             ret, buffer = cv2.imencode('.jpg', text_frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
-            frame = buffer.tobytes()
         else:
             frame = cv2.resize(frame, (640, 480))
             hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            lower_bound = np.array([20, 100, 100])
-            upper_bound = np.array([40, 255, 255])
-            mask = cv2.inRange(hsv_frame, lower_bound, upper_bound)
+            mask = cv2.inRange(hsv_frame, np.array([20, 100, 100]), np.array([40, 255, 255]))
             kernel = np.ones((5, 5), np.uint8)
-            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
             mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
             contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-            for idx, contour in enumerate(contours):
+            for contour in contours:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-                key_index = int((x + w / 2) / (640 / len(piano_keys)))
+                key_index = int((x + w / 2) * len(piano_keys) / 640) % len(piano_keys)
                 cv2.putText(frame, piano_keys[key_index], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
 
             frame = cv2.GaussianBlur(frame, (5, 5), 0)
             ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
-            frame = buffer.tobytes()
 
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
 @app.route('/')
 def index():
